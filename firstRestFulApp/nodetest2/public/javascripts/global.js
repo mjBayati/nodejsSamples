94e780c749e5728de87bb1b6ffc5c5d7627f5318
mjBayati <m.javad.bayati97@gmail.com>
2019-02-01 16:21:17 +0330

    new attr addesd for ajax calling in js

--- firstRestFulApp/nodetest2/public/javascripts/global.js
+++ firstRestFulApp/nodetest2/public/javascripts/global.js
@@ -3,10 +3,11 @@ var userListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
-
+  var userListData;
   // Populate the user table on initial page load
   populateTable();
 
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 });
 
 // Functions =============================================================
@@ -20,6 +21,7 @@ function populateTable() {
   // jQuery AJAX call for JSON
   $.getJSON( '/users/userlist', function( data ) {
 
+    userListData = data;
     // For each item in our JSON, add a table row and cells to the content string
     $.each(data, function(){
       tableContent += '<tr>';
@@ -31,6 +33,23 @@ function populateTable() {
 
     // Inject the whole content string into our existing HTML table
     console.log(tableContent);
-    $('userList table tbody').html(tableContent);
+    $('#userList table tbody').html(tableContent);
   });
 };
+
+function showUserInfo(event){
+    event.preventDefault();
+    var userName = $(this).attr('rel');  
+    var userArrayIndex = userListData.map(function(arrayItem){ return arrayItem.username;}).indexOf(userName);
+
+    console.log('+++',userListData);
+
+    var currentUserObjects = userListData[userArrayIndex];
+
+    console.log('-----',currentUserObjects);
+
+    $('#userInfoName').text(currentUserObjects.fullname);
+    $('#userInfoAge').text(currentUserObjects.age);
+    $('#userInfoGender').text(currentUserObjects.gender);
+    $('#userInfoLocation').text(currentUserObjects.location);
+  };
